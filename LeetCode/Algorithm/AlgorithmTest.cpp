22a84036c04022e63d06b672d5014de015bde5f2
Wu-Ke-CUC <wuke0704@outlook.com>
2026-04-19 11:01:21 +0800

    Test88

--- LeetCode/Algorithm/AlgorithmTest.cpp
+++ LeetCode/Algorithm/AlgorithmTest.cpp
@@ -2368,6 +2368,31 @@ class Solution
 			return smallHead->next;
 		}
 		#pragma endregion
+		#pragma region 88.合并两个有序数组
+		void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+			int index1 = m - 1, index2 = n - 1;
+			int index = m + n - 1;
+			while (index1 >= 0 || index2 >= 0)
+			{
+				if (index1 == -1)
+				{
+					nums1[index--] = nums2[index2--];
+				}
+				else if (index2 == -1)
+				{
+					nums1[index--] = nums1[index1--];
+				}
+				else if (nums1[index1] > nums2[index2])
+				{
+					nums1[index--] = nums1[index1--];
+				}
+				else
+				{
+					nums1[index--] = nums2[index2--];
+				}
+			}
+		}
+		#pragma endregion
 
 };
 int main()
